a56811088b332df6c053665d482ee99d14ec9968
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-04-06 02:31:51 +0800

    minor update

--- src/app/createListing/page.tsx
+++ src/app/createListing/page.tsx
@@ -2,7 +2,6 @@ import { PropertyForm } from "@/components/ListingForm";
 import { Orbitron } from "next/font/google";
 import MobileNavbar from "../components/navbar";
 
-
 const orbitron = Orbitron({
   subsets: ["latin"],
   weight: "400", // adjust as needed
@@ -10,14 +9,21 @@ const orbitron = Orbitron({
 
 export default function List() {
   return (
-    <main
-      className={`${orbitron.className} flex min-h-screen flex-col items-center justify-center p-8 bg-white text-black`}
-    >
-      <div className=" max-w-3xl w-full text-sm">
-        <h1 className="text-4xl font-bold text-center mb-8 ">List a Property</h1>
+    <div className="min-h-screen bg-white text-gray-800">
+      <header className="bg-zinc-100 text-black py-12 px-4 fixed w-full top-0 left-0 z-20 ">
+        <div className="max-w-6xl mx-auto">
+          <div className="flex items-center justify-center">
+            <h1 className="text-3xl md:text-4xl font-bold text-center">
+              List Your Property <br />
+              <span className="block text-center">On-Chain</span>
+            </h1>
+          </div>
+        </div>
+      </header>
+      <div className="mt-48 px-4 md:px-8 lg:px-16 xl:px-24">
         <PropertyForm />
       </div>
-      <MobileNavbar/>
-    </main>
+      <MobileNavbar />
+    </div>
   );
 }
--- src/app/stakeListedProject/page.tsx
+++ src/app/stakeListedProject/page.tsx
@@ -39,35 +39,45 @@ export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
 
   const filteredProjects = projects.filter((project) =>
-    (project.name + project.symbol).toLowerCase().includes(searchQuery.toLowerCase())
+    (project.name + project.symbol)
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase())
   );
 
   return (
-    <div className="px-4 py-4 pt-20">
-      {/* Fixed Search Bar */}
-      <div className="mb-6 fixed top-0 left-0 right-0 z-10 bg-white p-4 shadow-md rounded-2xl">
-        <input
-          type="text"
-          placeholder="Search by name or symbol..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-full px-4 py-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+    <div className="min-h-screen bg-white text-gray-800">
+      <header className="bg-zinc-100 text-black py-12 px-4 fixed w-full top-0 left-0 z-20 ">
+        <div className="max-w-6xl mx-auto">
+          <div className="flex items-center justify-center ">
+            <h1 className="text-3xl md:text-4xl font-bold">Invest</h1>
+          </div>
+        </div>
+      </header>
+      <div className="mt-58 px-4 md:px-8 lg:px-16 xl:px-24">
+        {/* Search Bar */}
+        <div className="mb-6 fixed top-32 left-0 right-0 z-10 bg-white p-4 shadow-md rounded-2xl">
+          <input
+            type="text"
+            placeholder="Search by name or symbol..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
 
-      {/* Filtered Projects */}
-      <div className="space-y-6 mt-4">
-        {filteredProjects.map((project) => (
-          <Link
-            href={`/stakeListedProject/${project.id}`}
-            key={project.id}
-            className="block"
-          >
-            <ProjectCard project={project} />
-          </Link>
-        ))}
+        {/* Filtered Projects */}
+        <div className="space-y-6 mt-4">
+          {filteredProjects.map((project) => (
+            <Link
+              href={`/stakeListedProject/${project.id}`}
+              key={project.id}
+              className="block"
+            >
+              <ProjectCard project={project} />
+            </Link>
+          ))}
+        </div>
       </div>
-
       <MobileNavbar />
     </div>
   );
